0e83e3b86d6c9afb7fb992b94aa8646cfb1b8322
Saket Kumar <mac2me.saket@gmail.com>
2026-05-26 09:46:48 +0530

    GFG segregate 0s and 1s

---
+++ src/main/java/twopointers/GFGSegregateZerosAndOnes.java
@@ -0,0 +1,30 @@
+package twopointers;
+
+public class GFGSegregateZerosAndOnes {
+    //Question has input in Arrays/ sorted or sorting(like here) will make easy/rearrange(like here)
+    public void segregate0and1(int[] arr) {
+        // code here
+        int lo = 0, hi = arr.length - 1;
+        while(lo < hi){
+            if(arr[lo] == 0){
+                lo++;
+            }else if(arr[hi] == 1){
+                hi--;
+            }else{
+                int temp = arr[lo];
+                arr[lo] = arr[hi];
+                arr[hi] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {0, 1, 0, 1, 0, 0, 1, 1, 1, 0};
+        GFGSegregateZerosAndOnes zerosAndOnes = new GFGSegregateZerosAndOnes();
+        zerosAndOnes.segregate0and1(arr);
+        for(int a: arr){
+            System.out.print(a + " ");
+        }
+
+    }
+}
