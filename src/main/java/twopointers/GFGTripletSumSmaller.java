e75b1bf7d1337c4206fabaa2af33e2fc913f27e4
Saket Kumar <mac2me.saket@gmail.com>
2026-05-30 23:56:20 +0530

    Solution to 3 Sum smaller to target

---
+++ src/main/java/twopointers/GFGTripletSumSmaller.java
@@ -0,0 +1,34 @@
+package twopointers;
+
+import java.util.Arrays;
+
+public class GFGTripletSumSmaller {
+
+    int countTriplets(int sum, int arr[]) {
+        // code here 1,3,4,5,7 - 12
+        Arrays.sort(arr);
+        int n = arr.length, count = 0;
+        for(int i = 0; i < n - 2; i++){
+            int lo = i + 1, hi = n - 1;
+            while(lo < hi){
+                int tripletSum = arr[i] + arr[lo] + arr[hi];
+                if(tripletSum < sum){
+
+                    count = count + (hi - lo);
+                    lo++;
+                }else{
+                    hi--;
+
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {-2, 0, 1, 3};
+        int target = 12;
+        GFGTripletSumSmaller tripletSumSmaller = new GFGTripletSumSmaller();
+        System.out.println(tripletSumSmaller.countTriplets(target, arr));
+    }
+}
