67271d75e263779429505737e5be526d0278226b
Saket Kumar <mac2me.saket@gmail.com>
2026-05-31 14:16:51 +0530

    Solving Dutch National Flag Problem

---
+++ src/main/java/twopointers/DutchNationalFlagLC75.java
@@ -0,0 +1,34 @@
+package twopointers;
+
+public class DutchNationalFlagLC75 {
+
+    public void sortColors(int[] arr) {
+        int lo = 0, mid = 0, hi = arr.length - 1;
+
+        while(mid <= hi){
+            if(arr[mid] == 0){
+                int temp = arr[lo];
+                arr[lo] = arr[mid];
+                arr[mid] = temp;
+                lo++;
+                mid++;
+            }else if(arr[mid] == 1){
+                mid++;
+            }else{
+                int temp = arr[hi];
+                arr[hi] = arr[mid];
+                arr[mid] = temp;
+                hi--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {2,0,2,1,1,0};
+        DutchNationalFlagLC75 lc75 = new DutchNationalFlagLC75();
+        lc75.sortColors(nums);
+        for (int num : nums) {
+            System.out.print(num);
+        }
+    }
+}
